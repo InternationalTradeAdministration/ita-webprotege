fe2a760344d27e09827caed507798dbb5497188b
lipsotiko <lipsotiko@gmail.com>
2020-06-19 16:31:32 -0400

    Updated webprotege source code to v4

--- run-local.sh
+++ run-local.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-#MongoDB
-sudo docker run --name ita-webprotege-db -d -v mongodb_data:/data/db itadevacr.azurecr.io/ita-webprotege/db:latest
-
-#Tomcat
-sudo docker run --name webprotege-web -d -v webprotege_data:/data/webprotege -v webprotege_logs:/var/log/webprotege --link ita-webprotege-db -p 8888:8080 itadevacr.azurecr.io/ita-webprotege/web:latest
+sudo docker-compose up
